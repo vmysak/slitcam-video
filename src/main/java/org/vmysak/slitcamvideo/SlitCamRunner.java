47f37e594caff4db9242599ce73765768d73f0e6
vmysak <moandorchik@gmail.com>
2017-02-27 23:05:52 +0200

    initial commit

--- src/main/java/org/vmysak/slitcamvideo/SlitCamRunner.java
+++ src/main/java/org/vmysak/slitcamvideo/SlitCamRunner.java
@@ -1,5 +1,6 @@
 package org.vmysak.slitcamvideo;
 
+import cv.Video;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -17,27 +18,28 @@ public class SlitCamRunner {
     }
 
     public static void main(String[] args) {
-        VideoCapture capture = new VideoCapture();
-        capture.open("/root/4/3.mov");
-        Mat frame = new Mat();
-        JFrame jframe = new JFrame("MyTitle");
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel vidpanel = new JLabel();
-        jframe.setContentPane(vidpanel);
-        jframe.setVisible(true);
-
-        while (true) {
-            if (capture.read(frame)) {
-                System.out.printf("s");
-                BufferedImage img = new BufferedImage(frame.width(), frame.height(), BufferedImage.TYPE_INT_ARGB_PRE);
-                byte[] data = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-                frame.get(0, 0, data);
-                ImageIcon image = new ImageIcon(img);
-                vidpanel.setIcon(image);
-                vidpanel.repaint();
-
-            }
-        }
+        Video.main(args);
+//        VideoCapture capture = new VideoCapture();
+//        capture.open("/root/4/3.mov");
+//        Mat frame = new Mat();
+//        JFrame jframe = new JFrame("MyTitle");
+//        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        JLabel vidpanel = new JLabel();
+//        jframe.setContentPane(vidpanel);
+//        jframe.setVisible(true);
+//
+//        while (true) {
+//            if (capture.read(frame)) {
+//                System.out.printf("s");
+//                BufferedImage img = new BufferedImage(frame.width(), frame.height(), BufferedImage.TYPE_INT_ARGB_PRE);
+//                byte[] data = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+//                frame.get(0, 0, data);
+//                ImageIcon image = new ImageIcon(img);
+//                vidpanel.setIcon(image);
+//                vidpanel.repaint();
+//
+//            }
+//        }
     }
 
 }
