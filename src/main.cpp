291a7519832c6393825735b9d597ac74b2c0bd61
vmysak <moandorchik@gmail.com>
2017-02-28 22:27:51 +0200

    initial commitsa

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+//    if (argc != 2) {
+//        printf("usage: slitcam <Image_Path>\n");
+//        return -1;
+//    }
+
+    cv:: cap("/root/4/3.MOV");
+    if (!cap.isOpened()) {
+        cout << "Can't open file";
+        return -1;
+    }
+
+    double fps = cap.get(CV_CAP_PROP_FPS);
+    cout << "Frames per second: " << fps << endl;
+
+    return 0;
+}
